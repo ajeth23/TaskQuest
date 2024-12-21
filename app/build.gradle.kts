d3ef09ec2d1e3159690fc1f35be141194aa8f671
Alan Jethro Ecuacion <119788178+ajeth23@users.noreply.github.com>
2024-12-21 23:49:55 +0800

    Update Dependencies and Gradle Files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.ecuacion.finalproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ecuacion.finalproject"
@@ -47,28 +47,30 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
-    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.firebase:firebase-auth-ktx:23.1.0")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("androidx.recyclerview:recyclerview:1.3.1") //viewing
-    implementation("com.google.firebase:firebase-firestore:24.7.1") // stoting
-    implementation("com.google.firebase:firebase-storage:20.2.1")   //updating
+    implementation("androidx.recyclerview:recyclerview:1.3.2") //viewing
+    implementation("com.google.firebase:firebase-firestore:25.1.1") // stoting
+    implementation("com.google.firebase:firebase-storage:21.0.1")   //updating
 
     implementation ("com.firebaseui:firebase-ui-database:8.0.0")
-    implementation ("com.google.firebase:firebase-database:20.2.2")
+    implementation ("com.google.firebase:firebase-database:21.0.0")
     implementation ("com.orhanobut:dialogplus:1.11@aar")
 
 
 
-    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.25")//gif
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.29")//gif
 
-    configurations.implementation {
-        exclude(group = "org.jetbrains.kotlin", module= "kotlin-stdlib-jdk8")
-    }
+//    configurations.implementation {
+//        exclude(group = "org.jetbrains.kotlin", module= "kotlin-stdlib-jdk8")
+//    }
+
+
 }
